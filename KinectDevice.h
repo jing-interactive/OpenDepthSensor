beac579d931bb5c9c739af4b348e67db7a0c299f
vinjn <vinjn.z@gmail.com>
2015-04-25 23:39:58 +0800

    Add Skeleton info

--- KinectDevice.h
+++ KinectDevice.h
@@ -4,21 +4,43 @@
 
 namespace Kinect
 {
-typedef std::shared_ptr<struct Device> DeviceRef;
+    typedef std::shared_ptr<struct Device> DeviceRef;
 
-struct Device
-{
+    struct Device
+    {
 #ifdef KINECT_V2
-    static DeviceRef createV2();
+        static DeviceRef createV2();
 #else
-    static DeviceRef createV1();
+        static DeviceRef createV1();
 #endif
 
-    virtual int getWidth() const = 0;
-    virtual int getHeight() const = 0;
+        virtual int getWidth() const = 0;
+        virtual int getHeight() const = 0;
 
-    ci::Channel16u depthChannel;
+        ci::Channel16u depthChannel;
+        ci::signals::signal<void()> signalDepthDirty;
 
-    ci::signals::signal<void()> signalDepthDirty;
-};
+        struct Skeleton
+        {
+            int id;
+            ci::vec2 pos;
+
+            struct Joint
+            {
+                enum
+                {
+                    LEFT_HAND,
+                    RIGHT_HAND,
+                    HEAD,
+                    JOINT_COUNT,
+                };
+                ci::vec2 pos;
+                float confidence;
+            };
+
+            Joint joints[Joint::JOINT_COUNT];
+        };
+        std::vector<Skeleton> skeletons;
+        ci::signals::signal<void()> signalSkeletonDirty;
+    };
 }
