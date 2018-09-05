87e04a0dfd4375ebfce342548ee19eccaf3878bb
vinjn <vinjn.z@gmail.com>
2018-09-05 21:22:39 +0800

    DeviceImi.cpp - depth channel completed

--- src/DeviceImi.cpp
+++ src/DeviceImi.cpp
@@ -25,11 +25,20 @@ namespace ds
 {
     struct DeviceImi : public Device
     {
+        ImiDeviceAttribute* pDeviceAttr = NULL;
+        ImiDeviceHandle pImiDevice = NULL;
+        // stream handles
+        uint32_t g_streamNum = 0;
+        ImiStreamHandle g_streams[10] = {NULL};
+        ImiStreamHandle depthHandle, colorHandle;
+        ivec2 depthSize;
+        ivec2 colorSize;
+
         virtual bool isValid() const { return pImiDevice != NULL; }
 
-        ivec2 getDepthSize() const { return depthChannel.getSize(); }
+        ivec2 getDepthSize() const { return depthSize; }
 
-        ivec2 getColorSize() const { return depthChannel.getSize(); }
+        ivec2 getColorSize() const { return colorSize; }
 
         virtual ~DeviceImi()
         {
@@ -62,7 +71,7 @@ namespace ds
             this->option = option;
 
             // 1.imiInitialize()
-            if (0 != imiInitialize(NULL))
+            if (imiInitialize(NULL))
             {
                 CI_LOG_E("ImiNect Init Failed!");
                 return;
@@ -82,7 +91,7 @@ namespace ds
             CI_LOG_I("Get %d Connected Imidevice.", deviceCount);
 
             // 3.imiOpenDevice()
-            if (0 != imiOpenDevice(pDeviceAttr[0].uri, &pImiDevice, 0))
+            if (imiOpenDevice(pDeviceAttr[0].uri, &pImiDevice, 0))
             {
                 CI_LOG_E("Open Imidevice Failed!");
                 return;
@@ -92,13 +101,40 @@ namespace ds
             // 4.imiOpenStream()
             if (option.enableDepth)
             {
-                if (0 != imiOpenStream(pImiDevice, IMI_DEPTH_FRAME, NULL, NULL,
-                                       &g_streams[g_streamNum++]))
+                if (imiOpenStream(pImiDevice, IMI_DEPTH_FRAME, NULL, NULL, &depthHandle))
                 {
                     CI_LOG_E("Open Depth Stream Failed!");
                     return;
                 }
+                g_streams[g_streamNum++] = depthHandle;
                 CI_LOG_I("Open Depth Stream Success.");
+
+                const ImiFrameMode* frameMode = imiGetCurrentFrameMode(pImiDevice, IMI_DEPTH_FRAME);
+                depthSize = {frameMode->resolutionX, frameMode->resolutionY};
+            }
+
+            if (option.enableColor)
+            {
+                {
+                    ImiFrameMode frameMode;
+                    frameMode.pixelFormat = IMI_PIXEL_FORMAT_IMAGE_RGB24;
+                    frameMode.resolutionX = IMAGE_WIDTH;
+                    frameMode.resolutionY = IMAGE_HEIGHT;
+                    frameMode.bitsPerPixel = DEFAULT_PERPIXEL_BITS;
+                    frameMode.framerate = DEFAULT_FRAMERATE;
+                    imiSetFrameMode(pImiDevice, IMI_COLOR_FRAME, &frameMode);
+                }
+
+                if (imiOpenStream(pImiDevice, IMI_COLOR_FRAME, NULL, NULL, &colorHandle))
+                {
+                    CI_LOG_E("Open Color Stream Failed!");
+                    return;
+                }
+                g_streams[g_streamNum++] = colorHandle;
+                CI_LOG_I("Open Color Stream Success.");
+
+                const ImiFrameMode* frameMode = imiGetCurrentFrameMode(pImiDevice, IMI_COLOR_FRAME);
+                colorSize = {frameMode->resolutionX, frameMode->resolutionY};
             }
 
             App::get()->getSignalUpdate().connect(std::bind(&DeviceImi::update, this));
@@ -106,20 +142,59 @@ namespace ds
 
         void update()
         {
-            if (option.enableDepth)
+            int32_t avStreamIndex;
+
+            // wait for stream, -1 means infinite;
+            if (imiWaitForStreams(g_streams, g_streamNum, &avStreamIndex, 100))
             {
-                // TODO: update depthChannel
+                return;
+            }
+
+            /*CI_LOG_E("channel index : %d", avStreamIndex);*/
+            if ((avStreamIndex < 0) || ((uint32_t)avStreamIndex >= g_streamNum))
+            {
+                CI_LOG_E("imiWaitForStream returns 0, but channel index abnormal: %d",
+                         avStreamIndex);
+                return;
+            }
+
+            if (option.enableDepth && g_streams[avStreamIndex] == depthHandle)
+            {
+                ImiImageFrame* pFrame = NULL;
+                // frame coming, read.
+                if (imiReadNextFrame(g_streams[avStreamIndex], &pFrame, 0))
+                {
+                    CI_LOG_E("imiReadNextFrame Failed, channel index : ") << avStreamIndex;
+                    return;
+                }
+                auto data = (uint16_t*)pFrame->pData;
+                depthChannel =
+                    Channel16u(depthSize.x, depthSize.y, sizeof(uint16_t) * depthSize.x, 1, data);
+
                 signalDepthDirty.emit();
+                imiReleaseFrame(&pFrame);
+            }
+
+            if (option.enableColor && g_streams[avStreamIndex] == colorHandle)
+            {
+                ImiImageFrame* pFrame = NULL;
+                // frame coming, read.
+                if (imiReadNextFrame(g_streams[avStreamIndex], &pFrame, 0))
+                {
+                    CI_LOG_E("imiReadNextFrame Failed, channel index : ") << avStreamIndex;
+                    return;
+                }
+                CI_ASSERT(pFrame->pixelFormat == IMI_PIXEL_FORMAT_IMAGE_RGB24);
+                auto data = (uint8_t*)pFrame->pData;
+                colorSurface =
+                    Surface8u(data, colorSize.x, colorSize.y, sizeof(uint8_t) * 3 * colorSize.x,
+                              SurfaceChannelOrder::RGB);
+                signalColorDirty.emit();
+                imiReleaseFrame(&pFrame);
             }
         }
-        ImiDeviceAttribute* pDeviceAttr = NULL;
-        ImiDeviceHandle pImiDevice = NULL;
-        // stream handles
-        uint32_t g_streamNum = 0;
-        ImiStreamHandle g_streams[10] = {NULL};
 
-        int width, height;
-    };
+    }; // namespace ds
 
     uint32_t getImiCount() { return 1; }
 
